b7b399f63c08ac15ec26f42b07d09510d9f68044
Kmrshubham335 <kumarshubham09133@gmail.com>
2023-07-29 17:35:01 +0530

    Recursion

--- Practice/p1.java
+++
@@ -1,7 +0,0 @@
-package Practice;
-
-public class p1{
-    public static void main(String args[]){
-        System.out.println("Practice file 1");
-    }
-}
---
+++ Recursion/Factorial.java
@@ -0,0 +1,19 @@
+package Recursion;
+//TIME COMPLEXITY:O(n).
+public class Factorial {
+    public static void main(String[] args) {
+        int n=5;
+        int result =FactorialNum(n);
+        System.out.println("The factorial of "+n+" is: " +result );
+    }
+
+    public static int FactorialNum(int n) {
+        int result;
+        if (n ==1 || n==0){         //base condition
+            return 1;
+        } 
+        else{
+            return n*FactorialNum(n-1);   //recursive call
+        }      
+    }
+}
