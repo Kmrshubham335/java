4648625610c58b41a1b2842dcf209bc32da2fa41
Kmrshubham335 <kumarshubham09133@gmail.com>
2023-08-24 21:16:33 +0530

    update equilibrium

--- Array/Equilibrium.java
+++ Array/Equilibrium.java
@@ -1,20 +1,23 @@
 package Array;
-//Equilibrium point of the array.
+
+// Equilibrium point of the array.
 public class Equilibrium {
     public static void main(String[] args) {
-        int arr[]={1,3,5,2,2};
-        int sum=0, rightsum=0;
-        int i;
-        for(i=0;i<arr.length;i++);{
-            sum+=arr[i];
-        }
-        for(i=0;i<arr.length;i++){
-            sum-=arr[i];
-            if(arr[i]==sum){
-            System.out.println("Equilibrium index is"+ i+1);
-            }
-            rightsum+=arr[i];
+        int arr[] = {1, 3, 5, 2, 2};
+        int sum = 0, leftSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
 
-    }    
+        for (int i = 0; i < arr.length; i++) {
+            sum -= arr[i];
+            
+            if (leftSum == sum) {
+                System.out.println("Equilibrium index is " + i);
+            }
+            
+            leftSum += arr[i];
+        }
+    }
 }
